6d56f0705c1e4b7a25e025a5762d5cc92f3df376
daniel mazzer <dmazzer@gmail.com>
2017-10-04 14:12:55 -0300

    adding file to begin future work

---
+++ src/main/java/com/grallandco/demos/SpeedAvg.java
@@ -0,0 +1,146 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.grallandco.demos;
+
+
+/*
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer09;
+import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
+import org.apache.flink.streaming.util.serialization.JSONDeserializationSchema;
+*/
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer09;
+import org.apache.flink.streaming.util.serialization.JSONDeserializationSchema;
+import org.apache.flink.util.Collector;
+
+import java.util.Properties;
+
+public class SpeedAvg {
+
+
+  public static void main(String[] args) throws Exception {
+    // create execution environment
+    StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+    Properties properties = new Properties();
+    properties.setProperty("bootstrap.servers", "localhost:9092");
+    properties.setProperty("group.id", "flink_consumer");
+
+
+    DataStream stream = env.addSource(
+            new FlinkKafkaConsumer09<>("flink-demo", 
+            new JSONDeserializationSchema(), 
+            properties)
+    );
+ 
+
+stream  .flatMap(new TelemetryJsonParser())
+            .keyBy(0)
+            .timeWindow(Time.seconds(10))
+            .reduce(new AvgReducer())
+            .flatMap(new AvgMapper())
+           .map(new AvgPrinter())
+            .print();
+
+    env.execute();
+  }
+    stream
+            .rebalance()
+/*            .map(new MapFunction<ObjectNode, String>() {
+                  private static final long serialVersionUID = -6867736771747690202L;
+
+                  @Override
+                  public String map(ObjectNode value) throws Exception {
+                    return "Stream Value: " + value.get("time").asText();;
+                  }
+                })
+*/
+            .map(new AvgPrinter())
+            .print();
+
+    env.execute();
+  }
+
+}
+
+
+    // Receive JSON data from Kafka broker and parse car number, speed and counter
+    class TelemetryJsonParser implements FlatMapFunction<ObjectNode, Tuple3<String, Float, Integer>> {
+      @Override
+      public void flatMap(ObjectNode jsonTelemetry, Collector<Tuple3<String, Float, Integer>> out) throws Exception {
+        // get elements
+        String carId = "car" + jsonTelemetry.get("id").asText();
+        float speed = jsonTelemetry.get("sensors").get("Speed").floatValue() * 18f/5f; // convert to kph
+        out.collect(new Tuple3<>(carId,  speed, 1 ));
+      }
+    }
+
+
+/**
+ * Add speed and count to prepare average
+ */
+class AvgReducer implements ReduceFunction<Tuple3<String, Float, Integer>> {
+  @Override
+  public Tuple3<String, Float, Integer> reduce(Tuple3<String, Float,Integer> value1, Tuple3<String, Float, Integer> value2) {
+    return new Tuple3<>(value1.f0, value1.f1 + value2.f1, value1.f2+1);
+  }
+}
+
+/**
+ * Calculate average from tuple
+ */
+class AvgMapper implements FlatMapFunction<Tuple3<String, Float, Integer>, Tuple2<String, Float>> {
+  @Override
+  public void flatMap(Tuple3<String, Float, Integer> carInfo, Collector<Tuple2<String, Float>> out) throws Exception {
+    out.collect(  new Tuple2<>( carInfo.f0 , carInfo.f1/carInfo.f2 )  );
+  }
+}
+
+
+class AvgPrinter implements MapFunction<Tuple2<String, Float>, String> {
+
+
+  @Override
+  public String map(Tuple2<String, Float> avgEntry) throws Exception {
+    return  String.format("Avg speed for %s : %.2f km/h ", avgEntry.f0 , avgEntry.f1 ) ;
+  }
+}
+
+class AvgPrinter implements MapFunction<ObjectNode, String> {
+    private static final long serialVersionUID = -6867736771747690202L;
+    @Override
+    public String map(ObjectNode jsonEvent) throws Exception {
+        return  String.format("jsonEvent : %s", jsonEvent.get("time")) ;
+    }
+}
+
+
+
